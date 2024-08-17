5af6d8d8e14f8fe8277d33464397c6e30aa67460
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-17 20:36:05 +0530

    Fix minor bugs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (fName) {
+    let reversed = fName.split('').reverse().join('');
+    
+    return reversed;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
