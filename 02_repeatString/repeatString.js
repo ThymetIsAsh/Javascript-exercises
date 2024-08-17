277d6e0eefa609dc7d91c98e327a605a05824ec4
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-17 19:28:52 +0530

    Add Function to repeat the String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let result="";
+    for (let i = 0; i < num; i++) {
+     result += string;
+     
+    }
+    return result;
 
 };
+
+repeatString('hey', 3);
+console.log (repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
