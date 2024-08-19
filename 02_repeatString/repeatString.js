edf44bfc9eed49e478a1820a58c74965e60a5739
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-20 01:56:39 +0530

    Add Loops to Repeat values

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,27 @@
 const repeatString = function (string, num) {
-    let result="";
+    if (num == -1) return "ERROR";
+    let result = "";
     for (let i = 0; i < num; i++) {
-     result += string;
-     
+        result += string;
+
     }
     return result;
 
 };
 
+const number = Math.floor(Math.random() * 1000);
+
 repeatString('hey', 3);
-console.log (repeatString);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('', 10);
+repeatString('odin', number);
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
