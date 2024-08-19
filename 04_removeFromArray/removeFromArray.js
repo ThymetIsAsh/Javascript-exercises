18ae201ccfd7acc61b8bdb5943eb39f0e7f70e48
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-20 01:21:11 +0530

    Remove items from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arrY, ...valuesToRemove) {
+        valuesToRemove.forEach(value => {
+            let index;
+            while ((index = arrY.indexOf(value)) !== -1) {
+                arrY.splice(index, 1);
+            }
+        });
+        return arrY;
+    };
+
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
