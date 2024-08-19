f075663681eeb17302f6322d01d97b876586764a
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-08-20 00:32:56 +0530

    Check Integers to Sum ALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (staRt, eNd) {
+    if (!Number.isInteger(staRt) || !Number.isInteger(eNd)) return "ERROR";
+    if (staRt < 0 || eNd < 0) return "ERROR";
+
+    let mIn,mAx,result=0;
+    mIn=Math.min(staRt,eNd)
+    mAx=Math.max(staRt,eNd)
+
+    for (let i = mIn; i <= mAx; i++) {
+        result += i;
+    }
+    return result;
 
 };
+
+sumAll(1,4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
