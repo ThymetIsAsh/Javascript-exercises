33d326708f4a72a2b5d6312071a8e7024d8fcb4e
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-09-07 23:49:45 +0530

    Add Functions to Perform various numeric operation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = (num)=> {
+    const value=Number(num);
+    if(value<0) return "OOPS"
+    let a = 0, b = 1;
+    for (let i = 2; i <= value; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return value === 0 ? a : b;
 };
 
 // Do not edit below this line
