2fe727228a5f338519b17c2498054170d028a94a
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-09-07 21:08:34 +0530

    Add functions to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function(a,b) {
-	return a+b;
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function(a,b) {
-	return a-b;
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+   let result=arr.reduce((total, item) => {
+    return total + item;
+  },0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result=arr.reduce((total, item) => {
+    return total * item;
+  },);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num,pow) {
+let result=Math.pow(num,pow);
+return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 
