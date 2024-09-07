c048428c8ddb7bffb35d6518de3d22f34af91898
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-09-08 00:38:44 +0530

    Add Function to Calculate
    Person with certine Conditions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+        const currentYear = new Date().getFullYear(); // Get the current year
+    
+        return people.reduce((oldest, person) => {
+            // Calculate age for each person
+            const ageOfPerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+            const ageOfOldest = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    
+            // Return the person with the greatest age
+            return ageOfPerson > ageOfOldest ? person : oldest;
+        });
+    };
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
