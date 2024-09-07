fe030fbbb4c96686cdbff83dcc99ddc7042b865d
Bhuvaneshwaran P <logeshbhuvanesh2123@gmail.com>
2024-09-07 21:54:55 +0530

    Add Functions Perform Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = (string) =>{
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const rmvStr = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
+let rev=rmvStr.split('').reverse().join('');
+return rmvStr===rev;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
